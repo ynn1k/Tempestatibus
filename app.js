b78349bf28a8bf2f11c5fd0d74edcfbe92dd460e
Yannik Hewlik <hi@yannik.dev>
2020-02-05 23:56:26 +0100

    ability to change location and init with stored location data

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+//init storage obj
+const storage = new Storage();
+//get stored location data
+const weatherLocation = storage.getLocationData();
 //init weather obj
-const weather = new Weather('Bochum', 'metric');
+const weather = new Weather(weatherLocation.city, 'metric');
+//init UI obj
 const ui = new UI();
 
 //get weather on DOM load
-document.addEventListener('DOMContentLoaded', getWeather)
+document.addEventListener('DOMContentLoaded', getWeather);
+
+//change location
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+   const city = document.getElementById('city').value;
+   weather.changeLocation(city);
+   //save location to localstorage
+   storage.setLocationData(city);
+   //update weather data
+   getWeather()
+});
+
 
 function getWeather() {
     weather.getWeather()
