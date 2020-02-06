a6b2c1b7d73dc443c00ec4eb57d051b171cbf3a6
Yannik Hewlik <hi@yannik.dev>
2020-02-06 19:43:26 +0100

    indentation 👉🏻

--- ui.js
+++ ui.js
@@ -1,28 +1,28 @@
 class UI {
     constructor() {
         this.descriptionMain =  document.getElementById('w-descMain');
-        this.description =  document.getElementById('w-desc');
-        this.temperature =  document.getElementById('w-string');
-        this.feelsLike =    document.getElementById('w-feels-like');
-        this.location =     document.getElementById('w-location');
-        this.humidity =     document.getElementById('w-humidity');
-        this.sunrise =      document.getElementById('w-sunrise');
-        this.sunset =       document.getElementById('w-sunset');
-        this.wind =         document.getElementById('w-wind');
-        this.icon =         document.getElementById('w-icon');
+        this.description =      document.getElementById('w-desc');
+        this.temperature =      document.getElementById('w-string');
+        this.feelsLike =        document.getElementById('w-feels-like');
+        this.location =         document.getElementById('w-location');
+        this.humidity =         document.getElementById('w-humidity');
+        this.sunrise =          document.getElementById('w-sunrise');
+        this.sunset =           document.getElementById('w-sunset');
+        this.wind =             document.getElementById('w-wind');
+        this.icon =             document.getElementById('w-icon');
     }
 
     render(weather) {
         console.log(weather);
-        this.descriptionMain.textContent    = weather.weather[0].main;
-        this.description.textContent    = weather.weather[0].description;
-        this.temperature.textContent    = `${weather.main.temp} °C`;
-        this.feelsLike.textContent      = `Feels like: ${weather.main.feels_like} °C`;
-        this.location.textContent       = weather.name + ', ' + weather.sys.country;
-        this.humidity.textContent       = `Humidity: ${weather.main.humidity} %`;
-        this.sunrise.textContent        = `Sunrise: ${this.formatTime(weather.sys.sunrise)} o'clock`;
-        this.sunset.textContent         = `Sunset: ${this.formatTime(weather.sys.sunset)} o'clock`;
-        this.wind.textContent           = `Wind: ${weather.wind.speed} m/sec.`;
+        this.descriptionMain.textContent =  weather.weather[0].main;
+        this.description.textContent =      weather.weather[0].description;
+        this.temperature.textContent =      weather.main.temp;
+        this.feelsLike.textContent =        weather.main.feels_like;
+        this.location.textContent =         weather.name + ', ' + weather.sys.country;
+        this.humidity.textContent =         weather.main.humidity;
+        this.sunrise.textContent =          this.formatTime(weather.sys.sunrise);
+        this.sunset.textContent =           this.formatTime(weather.sys.sunset);
+        this.wind.textContent =             weather.wind.speed;
         this.icon.setAttribute('src', `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
     }
 
